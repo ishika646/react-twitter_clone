439bb656266a01eb07f3bba27dc5a2d63c4ce0d8
ishika646 <igoyal221@gmail.com>
2021-08-15 11:05:35 +0530

    create a react app and publish it to github pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import LeftLayout from './Layouts/LeftLayout/LeftLayout';
+import MainLayout from './Layouts/MainLayout/MainLayout';
+import RightLayout from './Layouts/RightLayout/RightLayout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <LeftLayout/>
+      <MainLayout/>
+      <RightLayout/>
     </div>
+    
   );
 }
 
